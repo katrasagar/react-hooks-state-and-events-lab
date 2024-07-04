05bb84072455d2d11f185e4357e6b037005a06c3
Your Name <aqatra6@gmail.com>
2024-07-04 10:57:48 +0300

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const itemClass = inCart ? "in-cart":"";
+  const buttonText = inCart ? "Remove from Cart":"Add to Cart"
+  
+  const handleDarkModeToggle = () => {
+    setInCart ((prevInCart) =>!prevInCart);
+
+
+  };
+
+  
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleDarkModeToggle}>{buttonText}</button>
     </li>
   );
 }
